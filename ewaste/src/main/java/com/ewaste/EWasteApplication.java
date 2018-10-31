51d7572ce20502322ce8fa5a4a61baa864948dd3
jahnavi <jahnavi@192.168.0.4>
2018-10-31 19:59:04 +0530

    initial commit

---
+++ ewaste/src/main/java/com/ewaste/EWasteApplication.java
@@ -0,0 +1,12 @@
+package com.ewaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWasteApplication.class, args);
+	}
+}
